67a2c69615eac6359a6c78da4b16fef5b5541945
George Louthan <duplico@dupli.co>
2021-07-24 14:37:48 -0500

    Add a little upside down rainbow image.

--- ccs_workspace/badge.lgbt/badge_drivers/tlc6983.h
+++ ccs_workspace/badge.lgbt/badge_drivers/tlc6983.h
@@ -9,6 +9,12 @@
 #define BADGE_DRIVERS_TLC6983_H_
 
 typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} rgbcolor_t;
+
+typedef struct {
     uint16_t red;
     uint16_t green;
     uint16_t blue;
